f5bed9a968d222e4ab1fe5df58cb3f3539835b66
pengqihe <pengqihe@babeltime.com>
2017-11-27 19:12:57 +0800

    COH第一提

---
+++ src/COH.lua
@@ -0,0 +1,17 @@
+---
+--- Created by ukyohpq.
+--- DateTime: 17/11/27 11:03
+---
+
+local COH = class("COH")
+
+function COH:ctor()
+
+end
+
+function COH:init()
+
+end
+
+
+return COH
---
+++ src/Main.lua
@@ -0,0 +1,10 @@
+---
+--- Created by ukyohpq.
+--- DateTime: 17/11/27 10:53
+---
+package.path = package.path .. ";../../../../usefulLib/lua/?.lua"
+require("Utils.Logger")
+require("class")
+
+
+logErr("hello")
